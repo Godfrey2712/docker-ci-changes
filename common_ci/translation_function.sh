c5b2cde154045d2cfbfac43443ff9f4f304bbc68
Godfrey <godfreyinyama@gmail.com>
2023-06-02 13:10:30 +0000

    Update translation_function.sh

--- common_ci/translation_function.sh
+++ common_ci/translation_function.sh
@@ -3,12 +3,14 @@
 pattern="\.\s"
 # The translation functions to search for, you can add more using a pipe
 functions="__|_e"
+# Exclude at least two dots and a space seen
+exclude_dots="\.{2,}\s"
 # Exclude brackets seen inside the translation function
 exclude_bracket="\([^()]+\)"
 # Word to exclude from the search (e.g., N.B., etc)
 exclude_word="[A-Za-z]\.[A-Za-z]\.\s"
 # Search for calls to translation functions with the pattern in the parameter
-filtered_matches=$(grep -rnoE "($functions)\([^()]*$pattern[^()]*\)" src/ | sed "s/$exclude_word//g; s/$exclude_bracket//g")
+filtered_matches=$(grep -rnoE "($functions)\([^()]*$pattern[^()]*\)" src/ | sed "s/$exclude_word//g; s/$exclude_bracket//g; s/$exclude_dots//g")
 # Further check
 matches=$(echo "$filtered_matches" | grep -E "($functions)\([^()]*$pattern[^()]*\)")
 # Check for any matches
